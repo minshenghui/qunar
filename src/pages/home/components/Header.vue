09da0d620bc7b1051f439257152af9a8c44f0e5c
minshenghui <1013525657@qq.com>
2020-03-12 14:22:05 +0800

    初始化样式变量

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -18,10 +18,11 @@ export default {
 };
 </script>
 <style lang="stylus" scoped>
+@import '~styles/varibles.styl'
 .header
   display: flex
   line-height: .86rem
-  background: #00bcd4
+  background: $bgColor
 
   .header-back 
     width: .64rem
